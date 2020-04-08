759f52df61b3b06dce9e00d8a8d50e66d3aeb471
Gendhika Mahesajayya <gendhika508@gmail.com>
2020-04-08 18:00:57 +0700

    'done'

---
+++ src/app/karyawan-form/karyawan-form.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import {  Karyawan } from '../karyawan';
+import { DataService } from '../data.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+@Component({
+  selector: 'app-karyawan-form',
+  templateUrl: './karyawan-form.component.html',
+  styleUrls: ['./karyawan-form.component.scss']
+})
+export class KaryawanFormComponent implements OnInit {
+  karyawan: Karyawan = {
+    _id: '',
+    nama: '',
+    divisi: 'it',
+    gajipokok: '',
+    tunjangan: ''
+  };
+  id = null;
+  error = false;
+  update = true;
+
+  constructor(
+    private _snackBar: MatSnackBar,
+    private ds: DataService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      // jika ada parameter id di URL
+      if (params.get('id')) {
+        this.id = params.get('id');
+
+        this.ds.getKaryawan(this.id).subscribe(
+          response => {
+            this.karyawan = response as Karyawan;
+          },
+          err => {
+            console.log(err);
+            this.error = true;
+          }
+        );
+      } else {
+        this.update = false;
+      }
+    });
+  }
+
+  postKaryawan() {
+    this.ds.postKaryawan(this.karyawan).subscribe(response => {
+      // tampilkan notifikasi
+      this.openSnackBar("Karyawan Added", null)
+      this.router.navigate(['/home']);
+    });
+  }
+
+  deleteKaryawan() {
+    this.ds.deleteKaryawan(this.karyawan).subscribe(
+      response => {
+        // tampilkan notifikasi
+        this.openSnackBar("Karyawan Deleted", null)
+        this.router.navigate(['/home']);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  updateKaryawan() {
+    this.ds.updateKaryawan(this.karyawan).subscribe(
+      response => {
+        // tampilkan notifikasi
+        this.openSnackBar("Karyawan Updated", null)
+        this.router.navigate(['/home']);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+}
